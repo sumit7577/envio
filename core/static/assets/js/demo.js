330b4ad8074578b62112d90474afc351a1e83d53
sumit7577 <asmitkumarr082@gmail.com>
2021-09-04 10:26:45 +0530

    deployment start

--- core/static/assets/js/demo.js
+++ core/static/assets/js/demo.js
@@ -18,8 +18,8 @@ Circles.create({
 //Notify
 $.notify({
 	icon: 'flaticon-alarm-1',
-	title: 'Atlantis Lite',
-	message: 'Free Bootstrap 4 Admin Dashboard',
+	title: 'Envio-Delivery',
+	message: 'One Stop solution for all your delivery problems',
 },{
 	type: 'info',
 	placement: {
